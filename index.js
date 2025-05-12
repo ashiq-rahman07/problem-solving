55ca0609c63faefc3a8e5f88f9407cba8c27bcbe
ashiq-rahman07 <web3.0.ashiq@gmail.com>
2025-05-12 21:48:15 +0600

    2,3problem done

--- index.js
+++ index.js
@@ -11,4 +11,35 @@ function getMaleNames(arr) {
   return names;
 }
 
-console.log(getMaleNames(people));
+// console.log(getMaleNames(people));
+
+
+const books = [
+  { title: "Marhava JS Maro Tabha", author: "Jhankar Mahbub", year: 2025 },
+  { title: "JavaScript Basics", author: "John", year: 2018 },
+  { title: "Learn React", author: "Maya", year: 2020 },
+  { title: "Node Advanced", author: "Arif", year: 2021 },
+];
+
+const getBookTitles =(bookArray)=>{
+    const newArray = bookArray.map(book => book.title);
+  return newArray
+}
+
+// console.log(getBookTitles(books));
+
+
+// Task: Sorting Cars by Year
+
+const cars = [
+  { make: "Toyota", model: "Corolla", year: 2015 },
+  { make: "Honda", model: "Civic", year: 2012 },
+  { make: "Tesla", model: "Model 3", year: 2021 },
+];
+
+const sortCarsByYear=(carArray) =>{
+    const shortingArray = carArray.sort((a, b) => a.year - b.year);
+  return shortingArray
+}
+
+console.log(sortCarsByYear(cars));
